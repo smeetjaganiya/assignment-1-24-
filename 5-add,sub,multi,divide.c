9656222554e514e5d5ff3dc0061b17730fb8762f
smeetjaganiya <smeetjaganiya@gmail.com>
2025-09-14 18:57:35 +0530

    Add files via upload

---
+++ 5-add,sub,multi,divide.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    
+    float a,b;
+    
+    printf("Enter Two Numbers");
+    
+    scanf("%f%f",&a,&b);
+    
+    printf("Addition=%.2f\n",a+b);
+    printf("Subtraction=%.2f\n",a-b);
+    printf("Multiplication=%.2f\n",a*b);
+    
+    if (b!=0)
+        printf("Division=%.2f\n",a/b);
+    else
+        printf("Division is not possible");
+    
+    
+}
